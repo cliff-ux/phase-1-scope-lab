3479aaedff013ce13b326ea0224b3ed9ad6eb671
cliff-ux <rydleecliff@gmail.com>
2024-07-07 13:25:24 +0300

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,20 @@
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'some new value'; 
+}
